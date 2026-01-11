ff8989f8a9641116e1296e26f65e024d185fe9ec
Michael Sipes <msipes@sipesautomation.com>
2026-01-10 23:32:41 -0500

    Backend: Disable Celery result backend

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -9,8 +9,7 @@ REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
 celery_app = Celery(
     "sipes_automation_worker",
-    broker=REDIS_URL,
-    backend=REDIS_URL
+    broker=REDIS_URL
 )
 
 celery_app.conf.update(
